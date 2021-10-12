7cb2b36d0d1fe93f2c25921a2223b44c8cd46b49
Jay <Jay@ALPHA-1>
2021-10-12 17:58:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC350H_Project1_Jadgesh_Inderjeet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
